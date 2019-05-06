7904ac421c523885484b0a10e65fe11aa533cda4
orthoexact <50316205+orthoexact@users.noreply.github.com>
2019-05-06 15:59:42 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 export_file_url = 'https://drive.google.com/uc?export=download&id=1ywLPtqgBuBElREODgxOQJdrHapWdMcDj'
-export_file_name = 'export.pkl'
+export_file_name = 'BearTestModel.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
